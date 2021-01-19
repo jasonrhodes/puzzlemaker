45c4f854a7697f670de737a305498821e0312e2d
Glitch (starter-react-router) <none>
2021-01-19 03:02:24 +0000

    🍥😻 Checkpoint ./app/components/Builder.jsx:251993/161 ./app/index.html:251993/60 ./app/app.jsx:251993/73

--- app/app.jsx
+++ app/app.jsx
@@ -1,15 +1,17 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const Route = require('react-router-dom').Route;
-const BrowserRouter = require('react-router-dom').BrowserRouter;
-const hashHistory = require('react-router-dom').hashHistory;
+// const Route = require('react-router-dom').Route;
+// const BrowserRouter = require('react-router-dom').BrowserRouter;
+// const hashHistory = require('react-router-dom').hashHistory;
+
+ReactDOM.render(<h1>OGWER</h1>);
 
 /* Import Components */
-const Builder = require('./components/Builder');
+// const Builder = require('./components/Builderd');
 
-ReactDOM.render((
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={Builder}/>
-    </div>
-  </BrowserRouter>), document.getElementById('main'));
+// ReactDOM.render((
+//   <BrowserRouter>
+//     <div>
+//       <Route exact path="/" component={Builder}/>
+//     </div>
+//   </BrowserRouter>), document.getElementById('main'));
