83ba9145850b08b2b3e3449db91322cac35ab514
Glitch (starter-react-router) <none>
2021-01-19 02:38:30 +0000

    🚓📍 Checkpoint ./app/components/Builder.jsx:251993/610

--- app/components/Builder.jsx
+++ app/components/Builder.jsx
@@ -1,9 +1,31 @@
 const React = require("react");
+const DEFAULT_SIZE = 15;
+
+const createBlankGrid = (size = DEFAULT_GRID) => {
+  const grid = [];
+  for (let i = 0; i <= size; i++) {
+    let row = [];
+    for let (j = 0; j <= size; j++) {
+      row.push('o');
+    }
+    grid.push(row);
+  }
+  
+  return grid;
+}
+
+const Puzzle = (props) => {
+  return (
+  
+  );
+}
 
 const Builder = function() {
   return (
     <div>
       <h1>Puzzle Builder</h1>
+      <h2>Title by Author</h2>
+      <Puzzle grid={createBlankGrid()} />
     </div>
   );
 };
