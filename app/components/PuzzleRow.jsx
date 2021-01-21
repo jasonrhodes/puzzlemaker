1e212b243451475b2ca73dc6b3c98b6e39563b74
Glitch (starter-react-router) <none>
2021-01-21 20:17:08 +0000

    🍶🎺 Checkpoint ./app/components/PuzzleCell.jsx:251993/2590 ./app/components/PuzzleRow.jsx:251993/1945

--- app/components/PuzzleRow.jsx
+++ app/components/PuzzleRow.jsx
@@ -1,14 +1,24 @@
 const React = require("react");
 const PuzzleCell = require("./PuzzleCell");
 
-const PuzzleRow = ({ row, columns, ...rest }) => {
+const PuzzleRow = ({
+  row,
+  columns,
+  getNextClueNumber,
+  puzzleState,
+  setPuzzleState,
+  setActiveCell
+}) => {
   return (
     <div class="puzzle-row">
       {columns.map((cell, i) => (
         <PuzzleCell
           row={row}
           column={i}
-          {...rest}
+          getNextClueNumber={getNextClueNumber}
+          puzzleState={puzzleState}
+          setPuzzleState={setPuzzleState}
+          setActiveCell={setActiveCell}
         />
       ))}
     </div>
