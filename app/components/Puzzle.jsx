1ae93bbf3578e5c4770883570f57f8c7c9a04327
Glitch (starter-react-router) <none>
2021-01-21 21:13:57 +0000

    🏡⭐️ Checkpoint ./app/components/Puzzle.jsx:251993/1242

--- app/components/Puzzle.jsx
+++ app/components/Puzzle.jsx
@@ -1,32 +1,35 @@
 const React = require("react");
 const PuzzleRow = require("./PuzzleRow");
 
+const PuzzleContext = React.createContext();
 
-
-const Puzzle = props => {
+const PuzzleContextProvider = ({ grid, children }) => {
   const [puzzleState, setPuzzleState] = React.useState({
     activeCell: [],
     direction: "across",
     word: [],
     grid: props.grid
-  }); // set via local storage at some point, for saved puzzles?
-  
+  });
   const setActiveCell = (row, column) => setPuzzleState({ ...puzzleState, activeCell: [row, column] });
   let clue = 0;
   const getNextClueNumber = () => {
     return clue += 1;
   };
+  
   return (
+    <Puzzle
+  )
+}
+
+const Puzzle = ({ grid }) => {
+  return (
+    <PuzzleContextProvider grid={grid}></PuzzleContextProvider>
     <div class="puzzle-grid">
       {props.grid.map((columns, i) => (
         <PuzzleRow
           key={`row-${i}`}
           row={i}
           columns={columns}
-          getNextClueNumber={getNextClueNumber}
-          puzzleState={puzzleState}
-          setPuzzleState={setPuzzleState}
-          setActiveCell={setActiveCell}
         />
       ))}
     </div>
