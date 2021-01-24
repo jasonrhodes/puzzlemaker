8a924942173acb721b63e0c8f1e314911c7b6bfb
Glitch (puzzlemaker) <none>
2021-01-24 03:21:57 +0000

    🎳🏠 Checkpoint ./app/components/Puzzle.jsx:27649419/2 ./app/components/Menu.jsx:27649419/740 ./app/components/Home.jsx:27649419/62 ./static/style.css:27649419/1028

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -1,15 +1,19 @@
 const React = require("react");
 const { Link } = require("react-router-dom");
 
-const { DesktopDownloadIcon, PencilIcon, UnlockIcon, HomeIcon } = require("@primer/octicons-react");
+const { DesktopDownloadIcon, PencilIcon, UnlockIcon, HomeIcon, LockIcon } = require("@primer/octicons-react");
+
+const toggleLock = e => {
+  
+};
 
 const Menu = ({}) => {
   return (
     <div class="menu">
       <Link to="/"><HomeIcon size={24} /></Link>
-      <Link to="/"><DesktopDownloadIcon size={24} /></Link>
-      <Link to="/"><PencilIcon size={24} /></Link>
-      <Link to="/"><UnlockIcon size={24} /></Link>
+      <Link><DesktopDownloadIcon size={24} /></Link>
+      <Link><PencilIcon size={24} /></Link>
+      <Link onClick={toggleLock()} ><UnlockIcon size={24} /></Link>
     </div>
   );
 };
