2fd0a570f319041a304d27838dac391b4b2eae55
Glitch (puzzlemaker) <none>
2021-01-24 05:38:09 +0000

    🐟🐏 Checkpoint ./static/style.css:27649419/4 ./package.json:27649419/1850 ./app/components/Menu.jsx:27649419/223

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -3,6 +3,8 @@ const { Link } = require("react-router-dom");
 
 const { DesktopDownloadIcon, PencilIcon, UnlockIcon, HomeIcon, LockIcon } = require("@primer/octicons-react");
 
+const { ReactTooltip } = require ("react-tooltip");
+
 const toggleLock = e => {
   
 };
@@ -10,7 +12,7 @@ const toggleLock = e => {
 const Menu = ({}) => {
   return (
     <div class="menu">
-      <Link to="/"><HomeIcon size={24} /></Link>
+      <Link data-tip="Home" to="/"><HomeIcon size={24} /></Link>
       <Link><DesktopDownloadIcon size={24} /></Link>
       <Link><PencilIcon size={24} /></Link>
       <Link onClick={toggleLock()} ><UnlockIcon size={24} /></Link>
