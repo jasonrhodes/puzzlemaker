71fa3607ee059106f3571cef632c7919167adf2c
Glitch (puzzlemaker) <none>
2021-01-24 02:12:51 +0000

    🌉🏟 Checkpoint ./app/components/Puzzle.jsx:251993/554 ./static/style.css:251993/161

--- app/components/Puzzle.jsx
+++ app/components/Puzzle.jsx
@@ -5,18 +5,17 @@ const { PuzzleContextProvider, PuzzleContext } = require("./PuzzleContext");
 const Puzzle = ({ initialGrid }) => {
   const [title, setTitle] = React.useState("Title");
   const [author, setAuthor] = React.useState("Author");
-  const handleTitleChange = e => setTitle(e.target.value);
+  const handleTitleChange = e => {
+    this.style.width = e.target.value.length + "ch";
+    setTitle(e.target.value);
+  }
   const handleAuthorChange = e => setAuthor(e.target.value);
   return (
     <PuzzleContextProvider initialGrid={initialGrid}>
       <div class="puzzle-info">
-        <span class="inline-content-editable" contenteditable={true} onInput={handleTitleChange}>
-          {title}
-        </span>
-        by
-        <span class="inline-content-editable" contenteditable={true} onInput={handleAuthorChange}>
-          {author}
-        </span>
+        <input class="inline-content-editable" style={{ width: "5ch" }} value={title} type="text" onKeyUp={e => handleTitleChange(e)} />
+        <span> by </span>
+        <input class="inline-content-editable" style={{ width: "6ch" }} value={author} type="text" onKeyUp={e => handleAuthorChange(e)} />
       </div>
       <PuzzleContext.Consumer>
         {puzzle => (
