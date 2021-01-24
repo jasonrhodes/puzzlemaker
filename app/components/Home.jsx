dba45841a17d7fc76987ee9c878a3e4263717a58
Glitch (puzzlemaker) <none>
2021-01-24 03:33:20 +0000

    🐏👝 Checkpoint ./static/style.css:27649419/683 ./app/components/Home.jsx:27649419/449 ./app/components/Puzzle.jsx:251993/241

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -6,7 +6,7 @@ const CreateLink = ({ rows, columns }) => {
     return null;
   }
   return (
-    <Link to={{ pathname: "/edit", state: { rows, columns }}}>Create {rows} x {columns} puzzle</Link>
+    <Link class="btn" to={{ pathname: "/edit", state: { rows, columns }}}>Create {rows} x {columns} puzzle</Link>
   );
 }
 
@@ -20,25 +20,26 @@ module.exports = () => {
     <React.Fragment>
       <h1 class="title">Puzzlemaker</h1>
       <h2>Create a New Puzzle</h2>
-
-      <select onChange={handleChange}>
-        <option disabled={true} selected={true}>-- Choose a grid size --</option>
-        <option value="15 x 15">15 x 15 (standard NYT, Mon-Sat)</option>
-        <option value="21 x 21">21 x 21 (standard NYT, Sun)</option>
-        <option value="8 x 8">8 x 8</option>
-        <option value="9 x 9">9 x 9</option>
-        <option value="10 x 10">10 x 10</option>
-        <option value="11 x 11">11 x 11</option>
-        <option value="12 x 12">12 x 12</option>
-        <option value="13 x 13">13 x 13</option>
-        <option value="14 x 14">14 x 14</option>
-        <option value="16 x 16">16 x 16</option>
-        <option value="17 x 17">17 x 17</option>
-        <option value="18 x 18">18 x 18</option>
-        <option value="19 x 19">19 x 19</option>
-        <option value="20 x 20">20 x 20</option>
-      </select>
-      <div>{gridSize ? <CreateLink rows={rows} columns={columns} /> : null}</div>
+      <div class="inline">
+        <select onChange={handleChange}>
+          <option disabled={true} selected={true}>-- Choose a grid size --</option>
+          <option value="15 x 15">15 x 15 (standard NYT, Mon-Sat)</option>
+          <option value="21 x 21">21 x 21 (standard NYT, Sun)</option>
+          <option value="8 x 8">8 x 8</option>
+          <option value="9 x 9">9 x 9</option>
+          <option value="10 x 10">10 x 10</option>
+          <option value="11 x 11">11 x 11</option>
+          <option value="12 x 12">12 x 12</option>
+          <option value="13 x 13">13 x 13</option>
+          <option value="14 x 14">14 x 14</option>
+          <option value="16 x 16">16 x 16</option>
+          <option value="17 x 17">17 x 17</option>
+          <option value="18 x 18">18 x 18</option>
+          <option value="19 x 19">19 x 19</option>
+          <option value="20 x 20">20 x 20</option>
+        </select>
+        <div>{gridSize ? <CreateLink rows={rows} columns={columns} /> : null}</div>
+      </div>
     </React.Fragment>
   )
 }
--- app/components/Puzzle.jsx
+++ app/components/Puzzle.jsx
@@ -6,16 +6,18 @@ const Puzzle = ({ initialGrid }) => {
   const [title, setTitle] = React.useState("Title");
   const [author, setAuthor] = React.useState("Author");
   const handleTitleChange = e => {
-    e.target.style.width = e.target.value.length + "ch";
+    e.preventDefault();
+    console.log("title change", { title, author, e, value: e.target.value, length: e.target.value.length });
+    e.target.style.width = `${e.target.value.length}ch`;
     setTitle(e.target.value);
   }
   const handleAuthorChange = e => setAuthor(e.target.value);
   return (
     <PuzzleContextProvider initialGrid={initialGrid}>
       <div class="puzzle-info">
-        <input class="inline-content-editable" style={{ width: "5ch" }} value={title} type="text" onKeyUp={e => handleTitleChange(e)} />
+        <input class="inline-content-editable" value={title} type="text" onChange={handleTitleChange} />
         <span> by </span>
-        <input class="inline-content-editable" style={{ width: "6ch" }} value={author} type="text" onKeyUp={e => handleAuthorChange(e)} />
+        <input class="inline-content-editable" value={author} type="text" onChange={handleAuthorChange} />
       </div>
       <PuzzleContext.Consumer>
         {puzzle => (
