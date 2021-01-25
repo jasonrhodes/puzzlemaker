44bad6825ec832be5568bbad36a1e20b1a63619c
Glitch (puzzlemaker) <none>
2021-01-25 03:25:37 +0000

    🏑👧 Checkpoint ./app/components/PuzzleMenu.jsx:27649419/265 ./app/components/utils.js:251993/84 ./app/components/PuzzleContext.jsx:251993/15

--- app/components/PuzzleContext.jsx
+++ app/components/PuzzleContext.jsx
@@ -24,7 +24,7 @@ const PuzzleContextProvider = ({ initialGrid, children }) => {
       return { across: emptyWord, down: emptyWord };
     }
 
-    if (!grid[row][column]) {
+    if (grid[row][column].isBlackSquare) {
       return { across: emptyWord, down: emptyWord };
     }
 
--- app/components/PuzzleMenu.jsx
+++ app/components/PuzzleMenu.jsx
@@ -3,7 +3,7 @@ const { Link } = require("react-router-dom");
 
 const { DesktopDownloadIcon, PencilIcon, UnlockIcon, HomeIcon, LockIcon, MirrorIcon } = require("@primer/octicons-react");
 
-const { ReactTooltip } = require ("react-tooltip");
+const { ReactTooltip } = require("react-tooltip");
 
 
 
@@ -15,15 +15,15 @@ const PuzzleMenu = ({puzzle}) => {
   
   const lockIcon = () => {
     if (puzzle.symmetry){
-      return < LockIcon size={24}/>;
+      return < LockIcon data-tip="Unlock symmetry" size={24}/>;
     } else {
-      return < UnlockIcon size={24}/>;
+      return < UnlockIcon data-tip="Lock symmetry" size={24}/>;
     };
   };
   
   return (
     <div class="menu">
-      <Link><DesktopDownloadIcon size={24} /><ReactTooltip /></Link>
+      <Link><DesktopDownloadIcon size={24} /></Link>
       <Link class="subicon" onClick={toggleLock} >{lockIcon()} <MirrorIcon size={12} /></Link>
     </div>
   );
