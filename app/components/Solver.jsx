b505a252a08d8ee95e628db6438b1d59c2d71bbd
Glitch (puzzlemaker) <none>
2021-01-26 04:31:05 +0000

    🙎🐆 Checkpoint ./app/components/Solver.jsx:27649419/309 ./static/style.css:27649419/50 ./app/components/PuzzleMenu.jsx:27649419/293

--- app/components/Solver.jsx
+++ app/components/Solver.jsx
@@ -1,11 +1,12 @@
 const React = require("react");
 
-const Solver = ({}) => {
+module.exports = () => {
   return (
-    <div class="puzzle-info">
-      <span>title by author</span>
-    </div>
+    <React.Fragment>
+      <h1 class="title">Puzzle<span class="accent-text-alt">solver</span></h1>
+      <div class="puzzle-info">
+        <span>title by author</span>
+      </div>
+    </React.Fragment>
   );
 };
-
-module.exports = Solver;
