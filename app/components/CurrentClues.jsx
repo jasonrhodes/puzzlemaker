868cda8565235f473b06bb4109654b36dffdda05
Glitch (puzzlemaker) <none>
2021-01-26 01:33:10 +0000

    🏐🚥 Checkpoint ./app/components/CurrentClues.jsx:27649419/156 ./static/style.css:27649419/824

--- app/components/CurrentClues.jsx
+++ app/components/CurrentClues.jsx
@@ -8,18 +8,20 @@ const convertAnswerToSquares = (clue) => {
 };
 
 const CurrentClues = ({ across, down }) => {
-  <div class="current-clues">
-    <div id="across">
-      <h3>1 Across:</h3>
-      <div class="current">{convertAnswerToSquares(across.word.toUpperCase())}</div>
-      <div class="suggestions"></div>
+  return (
+    <div class="current-clues">
+      <div id="across">
+        <h3>1 Across:</h3>
+        <div class="current">{convertAnswerToSquares(across.word.toUpperCase())}</div>
+        <div class="suggestions"></div>
+      </div>
+      <div id="down">
+        <h3>1 Down:</h3>
+        <div class="current">{convertAnswerToSquares(down.word.toUpperCase())}</div>
+        <div class="suggestions"></div>
+      </div>
     </div>
-    <div id="down">
-      <h3>1 Down:</h3>
-      <div class="current">{convertAnswerToSquares(down.word.toUpperCase())}</div>
-      <div class="suggestions"></div>
-    </div>
-  </div>
+  );
 }
 
 module.exports = CurrentClues;
