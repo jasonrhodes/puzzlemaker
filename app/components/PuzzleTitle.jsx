bb3cc762c07be5c3e8e4d8e535c1b2d36008bdbd
Glitch (puzzlemaker) <none>
2021-01-26 08:41:00 +0000

    👼🍡 Checkpoint ./app/components/PuzzleTitle.jsx:27664858/326

--- app/components/PuzzleTitle.jsx
+++ app/components/PuzzleTitle.jsx
@@ -4,6 +4,8 @@ const { measureMyInputText } = require("./utils");
 const PuzzleTitle = () => {
   const [title, setTitle] = React.useState("Untitled");
   const [author, setAuthor] = React.useState("Author");
+  let width = 160;
+  
   const handleTitleChange = e => {
     e.preventDefault();
     e.target.style.width = measureMyInputText(e.target.id) + 'px';
@@ -11,14 +13,18 @@ const PuzzleTitle = () => {
   }
   const handleAuthorChange = e => {
     e.preventDefault();
-    e.target.style.width = measureMyInputText(e.target.id) + 'px';
+    let measuredWidth = measureMyInputText(e.target.id)
+    e.target.style.width =  + 'px';
     setAuthor(e.target.value);
   }
   return (
-    <div class="puzzle-info">
-      <input id="title" class="inline-content-editable" style={{ width: '66px' }} value={title} type="text" onChange={handleTitleChange} />
-      <span>by </span>
-      <input id="author" class="inline-content-editable" style={{ width: '57px' }} value={author} type="text" onChange={handleAuthorChange} />
+    <div>
+      <div class="puzzle-info">
+        <input id="title" class="inline-content-editable" style={{ width: '66px' }} value={title} type="text" onChange={handleTitleChange} />
+        <span>by </span>
+        <input id="author" class="inline-content-editable" style={{ width: '57px' }} value={author} type="text" onChange={handleAuthorChange} />
+      </div>
+      {width > 200 ? <br/> : null}
     </div>
   );
 }
