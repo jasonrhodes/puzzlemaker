3c9fcced9c5cbdffe8116ee2554623e98643c873
Glitch (puzzlemaker) <none>
2021-01-27 01:29:45 +0000

    🖐👱 Checkpoint ./app/components/Builder.jsx:251993/70

--- app/components/Builder.jsx
+++ app/components/Builder.jsx
@@ -5,18 +5,18 @@ const { PuzzleContextProvider, PuzzleContext } = require("./PuzzleContext");
 
 const { initGrid } = require("./utils");
 
-const Builder = function({ location }) {
+const Builder = function({ location, match }) {
   const { rows, columns } = location.state || {};
   const grid = initGrid({ rows: rows || 15, columns: columns || 15 });
   // const { puzzleId } = useParams();
-  console.log('location', location);
+  console.log('route', { location, match });
   
   const handleClick = (puzzle) => {
     puzzle.setActiveCell([]);
   }
   
   return (
-    <PuzzleContextProvider initialGrid={grid}>
+    <PuzzleContextProvider initialGrid={grid} puzzleId={match.params.puzzleId}>
       <PuzzleContext.Consumer>
         {puzzle => (
           <div class="container" onClick={()=>handleClick(puzzle)}>
