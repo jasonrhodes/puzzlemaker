1f8014600ee9ce2af86dfa06f0dffbae256a39c4
Glitch (puzzlemaker) <none>
2021-01-27 06:17:15 +0000

    🚴🐞 Checkpoint ./static/style.css:27649419/19 ./app/components/SavedPuzzleList.jsx:27649419/705

--- app/components/SavedPuzzleList.jsx
+++ app/components/SavedPuzzleList.jsx
@@ -15,11 +15,21 @@ module.exports = function SavedPuzzleList() {
     }));
   }, []);
   
+  const storageDelete = e => {
+    e.preventDefault();
+    console.log(Object.keys(window.localStorage));
+    console.log(e.target.id);
+    console.log(e.target.id.replace('del',''));
+    window.localStorage.removeItem(e.target.id.replace('del',''));
+    console.log(Object.keys(window.localStorage));
+  }
+    
+  
   return (
     <ul className="saved-puzzle-list">
       {puzzles.map(({ id, title, author }) => (
         <li key={id}>
-          <Link to={"/edit/" + id}>{title} by {author}</Link> <a><TrashIcon /></a>
+          <Link to={"/edit/" + id}>{title} by {author}</Link> <a id={'del' + id} onClick={(e)=>{storageDelete}}><TrashIcon /></a>
         </li>
       ))}
     </ul>
