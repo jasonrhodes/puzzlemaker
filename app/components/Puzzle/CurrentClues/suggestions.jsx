8248ba60feb1df51d3a460e238a3abc56e218a18
Glitch (puzzlemaker) <none>
2021-01-31 06:08:46 +0000

    🐄💐 Checkpoint ./app/components/Puzzle/CurrentClues/suggestions.jsx:27649419/1834 ./static/style.css:27649419/5

--- app/components/Puzzle/CurrentClues/suggestions.jsx
+++ app/components/Puzzle/CurrentClues/suggestions.jsx
@@ -57,55 +57,6 @@ async function getSuggestions(clue, setFunc) {
   return matches;
 }
 
-// function suggestionsList({ suggestions }) {
-//   return (
-//     <div class="suggestions">
-//       {filterSuggestions(suggestions, "across").map((x, i) => (
-//         <div class="inline">
-//           <div
-//             onMouseEnter={e => highlightCrosses(e, "across")}
-//             onMouseLeave={e => unHighlightCrosses(e)}
-//             class={
-//               acrossHighlight ==
-//                 x[puzzle.activeCell[1] - across.range[0]] ||
-//               x == downFilter[0]
-//                 ? "suggestion highlighted"
-//                 : !downSuggestions.length ||
-//                   downSuggestions
-//                     .map(s => s.text[puzzle.activeCell[0] - down.range[0]])
-//                     .includes(x[puzzle.activeCell[1] - across.range[0]])
-//                 ? "suggestion"
-//                 : "suggestion unmatched"
-//             }
-//             onClick={e => fillWithSuggestion(e, x, "across")}
-//           >
-//             {x}
-//           </div>
-//           <a onClick={e => hideNonCrosses(e, "across")}>
-//             <ArrowDownIcon size={12} />
-//             <span class="pbtip">
-//               <b>
-//                 {x == downFilter[0]
-//                   ? "Unfilter Down crosses"
-//                   : "Filter Down crosses"}
-//               </b>
-//             </span>
-//           </a>
-//           <a target="_blank" href={"http://onelook.com/?w=" + x}>
-//             <img
-//               style={{ width: "12px" }}
-//               src="https://cdn.glitch.com/7a2e2b2d-f058-4f81-950d-8b81f72c14fc%2Fonelook.png?v=1611800262010"
-//             />
-//             <span class="pbtip">
-//               <b>Open in OneLook</b>
-//             </span>
-//           </a>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
 function SuggestionsList({
   ad,
   puzzle,
@@ -116,8 +67,7 @@ function SuggestionsList({
   setOtherFilter
 }) {
   const cur_word = ad == "down" ? puzzle.words.down : puzzle.words.across;
-  const active_letter =
-    ad == "down" ? puzzle.activeCell[0] : puzzle.activeCell[1];
+  const active_letter = ad == "down" ? puzzle.activeCell[0] : puzzle.activeCell[1];
   const position = active_letter - cur_word.range[0];
   const [mySuggestions, setMySuggestions] = React.useState([]);
 
@@ -173,7 +123,6 @@ function SuggestionsList({
     }
     puzzle.setGrid(newGrid);
 
-    setOtherFilter([]);
     setOtherFilter([]);
     setOtherHighlight(null);
     focusOnActive();
