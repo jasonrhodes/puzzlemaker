eeab8cabde8ce637e80a2e26d6e8437b6e477f38
Glitch (puzzlemaker) <none>
2021-01-31 07:53:08 +0000

    🌯🌱 Checkpoint ./app/components/Puzzle/Title.jsx:27649419/731

--- app/components/Puzzle/Title.jsx
+++ app/components/Puzzle/Title.jsx
@@ -2,21 +2,18 @@ const React = require("react");
 const { measureMyInputText } = require("../../utils/style");
 
 const PuzzleTitle = ({ width, title, author, setTitle, setAuthor }) => {
-  const [authorWidth, setAuthorWidth] = React.useState(60);
-  const [titleWidth, setTitleWidth] = React.useState(75);
   
-  const handleChange = (e, setFunc, setWidthFunc) => {
-    console.log("setting value", e.target.value);
+  const handleChange = (e, setFunc) => {
     e.preventDefault();
     setFunc(e.target.value);
   }
 
   return (
-    <div style={{'margin-right': 'calc(100% - ' + (authorWidth + titleWidth + 235) + 'px)'}}>
+    <div style={{'margin-right':  (width > (measureMyInputText(author) + measureMyInputText(title) + 235) ? ('calc(100% - ' + (measureMyInputText(author) + measureMyInputText(title) + 235) + 'px)') : '0px')}}>
       <div class="puzzle-info" id="puzzle-info">
-        <input id="title" class="inline-content-editable" style={{ width: measureMyInputText(title) + 'px' }} value={title} type="text" onChange={(e) => handleChange(e, setTitle, setTitleWidth)} />
+        <input id="title" class="inline-content-editable" style={{ width: measureMyInputText(title) + 'px' }} value={title} type="text" onChange={(e) => handleChange(e, setTitle)} />
         <span>by </span>
-        <input id="author" class="inline-content-editable" style={{ width: measureMyInputText(author) + 'px' }} value={author} type="text" onChange={(e) => handleChange(e, setAuthor, setAuthorWidth)} />
+        <input id="author" class="inline-content-editable" style={{ width: measureMyInputText(author) + 'px' }} value={author} type="text" onChange={(e) => handleChange(e, setAuthor)} />
       </div>
     </div>
   );
