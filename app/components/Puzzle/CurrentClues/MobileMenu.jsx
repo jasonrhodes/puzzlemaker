2c691efcbf944e7a9712d2ee0c76df2acf104531
Glitch (puzzlemaker) <none>
2021-02-01 03:23:25 +0000

    😬💁 Checkpoint ./static/style.css:27649419/660 ./app/components/Puzzle/CurrentClues/index.jsx:251993/2344 ./app/components/Puzzle/CurrentClues/MobileMenu.jsx:251993/3423

--- app/components/Puzzle/CurrentClues/MobileMenu.jsx
+++ app/components/Puzzle/CurrentClues/MobileMenu.jsx
@@ -1,23 +1,81 @@
 const React = require("react");
 const { TypographyIcon, TriangleLeftIcon, TriangleRightIcon } = require("@primer/octicons-react");
 
+const navClue = (e, pn) => {
+  e.stopPropagation();
+  var newGrid = puzzle.grid;
+  var terminus, termrow, termcol;
+  var ad = puzzle.direction;
+  if (pn == 'next') {
+    //find the start with the next highest number
+    for (let row = 0; row < newGrid.length; row++) {
+      for (let column = 0; column < newGrid[row].length; column++) {
+        var clue = newGrid[row][column].clue;
+        if ((ad == "down" && clue.isDownStart && clue.downClueNumber > downNumber) ||
+            (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber > acrossNumber)) {
+          puzzle.setActiveCell([row, column]);
+          return;
+        } else if ((!terminus && ad == "down" && clue.isDownStart) ||
+                   (!terminus && ad == "across" && clue.isAcrossStart) ||
+                   (ad == "down" && clue.isDownStart && clue.downClueNumber < terminus) ||
+                   (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber < terminus)) {
+          terminus = ad == "down" ? clue.downClueNumber : clue.acrossClueNumber;
+          termrow = row;
+          termcol = column;
+        }
+      }
+    }
+  } else {
+    //find the start with the next lowest number
+    for (let row = newGrid.length - 1; row >= 0; row--) {
+      for (let column = newGrid[row].length - 1; column >= 0; column--) {
+        var clue = newGrid[row][column].clue;
+        if ((ad == "down" && clue.isDownStart && clue.downClueNumber < downNumber) ||
+            (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber < acrossNumber)) {
+          puzzle.setActiveCell([row, column]);
+          return;
+        } else if ((!terminus && ad == "down" && clue.isDownStart) ||
+                   (!terminus && ad == "across" && clue.isAcrossStart) ||
+                   (ad == "down" && clue.isDownStart && clue.downClueNumber < terminus) ||
+                   (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber < terminus)) {
+          terminus = ad == "down" ? clue.downClueNumber : clue.acrossClueNumber;
+          termrow = row;
+          termcol = column;
+        }
+      }
+    }
+  }
+  console.log(terminus);
+  console.log(row);
+  console.log(column);
+  puzzle.setActiveCell([termrow, termcol]);
+  return;
+};
+
 module.exports = function MobileMenu({
   mobileView,
   setMobileView,
   acrossNumber,
   acrossWord,
   downNumber,
-  downWord,
-  navClue
+  downWord
 }) {
   const keyBoardSwitch = (e, view) => {
     e.stopPropagation();
     setMobileView(view);
   };
+  
+  const handlePrevClueClick = e => {
+    navClue(e, "prev");
+  };
+  
+  const handleNextClueClick = e => {
+    navClue(e, "next");
+  };
 
   return (
     <div id="mobilemenu">
-      <a class="cluenav prev key" onClick={e => {navClue(e, "prev");}}>
+      <a class="cluenav prev key" onClick={handlePrevClueClick}>
         <TriangleLeftIcon size={24} />
       </a>
       <a class={mobileView == "keyboard" ? "activemobile" : ""}onClick={e => keyBoardSwitch(e, "keyboard")}>
@@ -35,7 +93,7 @@ module.exports = function MobileMenu({
       >
         {downNumber !== '-' ? downNumber + 'A: ' + downWord : '-' }
       </a>
-      <a class="cluenav next key" onClick={e => { navClue(e,"next");}}>
+      <a class="cluenav next key" onClick={handleNextClueClick}>
         <TriangleRightIcon size={24} />
       </a>
     </div>
--- app/components/Puzzle/CurrentClues/index.jsx
+++ app/components/Puzzle/CurrentClues/index.jsx
@@ -27,56 +27,7 @@ const CurrentClues = ({ across, down, puzzle }) => {
       setAcrossFilter([]);
     }
   };
-  const navClue = (e, pn) => {
-    e.stopPropagation();
-    var newGrid = puzzle.grid;
-    var terminus, termrow, termcol;
-    var ad = puzzle.direction;
-    if (pn == 'next') {
-      //find the start with the next highest number
-      for (let row = 0; row < newGrid.length; row++) {
-        for (let column = 0; column < newGrid[row].length; column++) {
-          var clue = newGrid[row][column].clue;
-          if ((ad == "down" && clue.isDownStart && clue.downClueNumber > downNumber) ||
-              (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber > acrossNumber)) {
-            puzzle.setActiveCell([row, column]);
-            return;
-          } else if ((!terminus && ad == "down" && clue.isDownStart) ||
-                     (!terminus && ad == "across" && clue.isAcrossStart) ||
-                     (ad == "down" && clue.isDownStart && clue.downClueNumber < terminus) ||
-                     (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber < terminus)) {
-            terminus = ad == "down" ? clue.downClueNumber : clue.acrossClueNumber;
-            termrow = row;
-            termcol = column;
-          }
-        }
-      }
-    } else {
-      //find the start with the next lowest number
-      for (let row = newGrid.length - 1; row >= 0; row--) {
-        for (let column = newGrid[row].length - 1; column >= 0; column--) {
-          var clue = newGrid[row][column].clue;
-          if ((ad == "down" && clue.isDownStart && clue.downClueNumber < downNumber) ||
-              (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber < acrossNumber)) {
-            puzzle.setActiveCell([row, column]);
-            return;
-          } else if ((!terminus && ad == "down" && clue.isDownStart) ||
-                     (!terminus && ad == "across" && clue.isAcrossStart) ||
-                     (ad == "down" && clue.isDownStart && clue.downClueNumber < terminus) ||
-                     (ad == "across" && clue.isAcrossStart && clue.acrossClueNumber < terminus)) {
-            terminus = ad == "down" ? clue.downClueNumber : clue.acrossClueNumber;
-            termrow = row;
-            termcol = column;
-          }
-        }
-      }
-    }
-    console.log(terminus);
-    console.log(row);
-    console.log(column);
-    puzzle.setActiveCell([termrow, termcol]);
-    return;
-  };
+  
   return (
     <div class="current-clues">
       <MobileMenu
@@ -86,7 +37,6 @@ const CurrentClues = ({ across, down, puzzle }) => {
         acrossWord={across.word.toUpperCase()}
         downNumber={downNumber}
         downWord={down.word.toUpperCase()}
-        navClue={navClue}
       />
       {acrossNumber !== "-" ? (
         <div id="across" class={mobileView == "across" ? "activemobile" : ""}>
