11371eec28c186dc126eeb41d8de7ebce25840f0
Glitch (puzzlemaker) <none>
2021-02-01 06:19:16 +0000

    🐷📖 Checkpoint ./app/components/Puzzle/Title.jsx:27649419/59 ./static/style.css:27649419/4

--- app/components/Puzzle/Title.jsx
+++ app/components/Puzzle/Title.jsx
@@ -12,9 +12,9 @@ const PuzzleTitle = ({ titleWidth, authorWidth, setTitleWidth, setAuthorWidth, t
   return (
     <div>
       <div class="puzzle-info" id="puzzle-info">{ titleWidth } { authorWidth }
-        <input id="title" placeholder="Untitled" class="inline-content-editable" style={{ width: ((title && title.length > 8) ? measureMyInputText(title) : measureMyInputText("Untitled"))+ "px" }} value={title} type="text" onChange={(e) => handleChange(e, setTitle, setTitleWidth)} />
+        <input id="title" placeholder="Untitled" class="inline-content-editable" style={{ width: (title.length ? measureMyInputText(title) : measureMyInputText("Untitled"))+ "px" }} value={title} type="text" onChange={(e) => handleChange(e, setTitle, setTitleWidth)} />
         <span>by </span>
-        <input id="author" placeholder="Author" class="inline-content-editable" style={{ width: ((author && author.length > 6) ? measureMyInputText(author) : measureMyInputText("Author"))+ "px" }} value={author} type="text" onChange={(e) => handleChange(e, setAuthor, setAuthorWidth)} />
+        <input id="author" placeholder="Author" class="inline-content-editable" style={{ width: (author.length ? measureMyInputText(author) : measureMyInputText("Author"))+ "px" }} value={author} type="text" onChange={(e) => handleChange(e, setAuthor, setAuthorWidth)} />
       </div>
     </div>
   );
