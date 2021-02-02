59ac405be5bfc78d5381a7655dabfcee11c06577
Glitch (puzzlemaker) <none>
2021-02-02 05:25:28 +0000

    🎍🚙 Checkpoint ./app/components/Solver.jsx:27649419/1375

--- app/components/Solver.jsx
+++ app/components/Solver.jsx
@@ -1,12 +1,26 @@
 const React = require("react");
+const Puzzle = require("./Puzzle");
+const { PuzzleContextProvider, PuzzleContext } = require("./Puzzle/Context");
+const initGrid = require("../utils/initGrid");
 
-module.exports = () => {
+const Solver = function({ location, match }) {
+  const { rows, columns } = location.state || {};
+  const grid = initGrid({ rows: rows || 15, columns: columns || 15 });
+
   return (
-    <React.Fragment>
-      <h1 class="title">Puzzle<span class="accent-text-alt">solver</span></h1>
-      <div class="puzzle-info">
-        <span>title by author</span>
-      </div>
-    </React.Fragment>
+    <PuzzleContextProvider initialGrid={grid} puzzleId={match.params.puzzleId}>
+      <PuzzleContext.Consumer>
+        {puzzle => (
+          <div class="container">
+            <h1 class="title">Puzzle<span class="accent-text-alt">solver</span></h1>
+            <div class="page">
+              <Puzzle initialGrid={grid} />
+            </div>
+          </div>
+        )}
+      </PuzzleContext.Consumer>
+    </PuzzleContextProvider>
   );
 };
+
+module.exports = Solver;
