020dbc268ae28caae3ee2e2b1cabb84fd8607c0c
Glitch (puzzlemaker) <none>
2021-02-03 16:26:14 +0000

    🌄🚞 Checkpoint ./app/components/Puzzle/Menu.jsx:27664858/8

--- app/components/Puzzle/Menu.jsx
+++ app/components/Puzzle/Menu.jsx
@@ -49,7 +49,7 @@ const PuzzleMenu = ({ puzzle }) => {
     let filename = puzzle.title ? puzzle.title+'.puz' : 'myPuz.puz';
     let serialized = convertPuzzleToJSON(puzzle);
     let fileContents = new PuzWriter().toPuz(serialized);
-    let file = new File([fileContents], filename);
+    let file = new Blob([fileContents], filename);
 
     element.href = URL.createObjectURL(file);
     //element.download = "myPuz.puz";
