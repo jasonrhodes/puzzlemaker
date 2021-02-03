5a55607abbca57cce256582fdec926544eb9c0be
Glitch (puzzlemaker) <none>
2021-02-03 14:26:13 +0000

    🎣💘 Checkpoint ./app/components/Puzzle/CurrentClues/index.jsx:27649419/381 ./static/style.css:27649419/540

--- app/components/Puzzle/CurrentClues/index.jsx
+++ app/components/Puzzle/CurrentClues/index.jsx
@@ -54,17 +54,6 @@ const CurrentClues = ({ across, down, puzzle }) => {
   //     }
   //   }
   // });
-    document.addEventListener('touchmove', function (event) {
-      if (event.scale !== 1) { event.preventDefault(); }
-    }, false);
-    var lastTouchEnd = 0;
-    document.addEventListener('touchend', function (event) {
-      var now = (new Date()).getTime();
-      if (now - lastTouchEnd <= 300) {
-        event.preventDefault();
-      }
-      lastTouchEnd = now;
-    }, false);
   
   return (
     <div class="current-clues" id="current-clues">
