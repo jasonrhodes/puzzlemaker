8dc65080c6292528012874c416e279d1ada13fc5
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2021-02-04 16:31:03 -0500

    Local updates

---
+++ app/utils/__test__/cellNavigation.test.js
@@ -0,0 +1,72 @@
+const {
+  getNextAcrossCellCoords,
+  getNextAcrossClueStart,
+  getPrevAcrossCellCoords,
+  getPrevAcrossClueStart,
+  getNextDownCellCoords,
+  getNextDownClueStart,
+  getPrevDownCellCoords,
+  getPrevDownClueStart,
+} = require("../cellNavigation");
+
+const gridMock = require("./gridMock");
+
+describe("get next across coords", () => {
+  test("basic next cell", () => {
+    expect(getNextAcrossCellCoords(0, 3, gridMock)).toEqual([0, 4]);
+  });
+  test("wrap around to the first cell in the next row", () => {
+    expect(getNextAcrossCellCoords(0, 6, gridMock)).toEqual([1, 0]);
+  });
+  test("wrap back to the beginning", () => {
+    expect(getNextAcrossCellCoords(6, 6, gridMock)).toEqual([0, 0]);
+  });
+});
+
+describe("get next across clue start", () => {
+  test("skipping a black square", () => {
+    expect(getNextAcrossClueStart(0, 2, gridMock)).toEqual([0, 4]);
+  });
+  test("starting on a black square", () => {
+    expect(getNextAcrossClueStart(0, 3, gridMock)).toEqual([0, 4]);
+  });
+  test("wrapping to the next row", () => {
+    expect(getNextAcrossClueStart(0, 4, gridMock)).toEqual([1, 1]);
+  });
+  test("wrapping to the next row with a starting black square", () => {
+    expect(getNextAcrossClueStart(2, 0, gridMock)).toEqual([4, 0]);
+  });
+  test("wrapping back to the start", () => {
+    expect(getNextAcrossClueStart(6, 4, gridMock)).toEqual([0, 0]);
+  });
+});
+
+describe("get previous across cell coordinates", () => {
+  test("basic coordinates", () => {
+    expect(getPrevAcrossCellCoords(0, 4, gridMock)).toEqual([0, 3]);
+  });
+  test("wrap back to the last cell in the previous row", () => {
+    expect(getPrevAcrossCellCoords(1, 0, gridMock)).toEqual([0, 6]);
+  });
+  test("wrap around to the end", () => {
+    expect(getPrevAcrossCellCoords(0, 0, gridMock)).toEqual([6, 6]);
+  });
+});
+
+describe("get prev across clue start", () => {
+  test("skipping a black square", () => {
+    expect(getPrevAcrossClueStart(0, 5, gridMock)).toEqual([0, 0]);
+  });
+  test("starting on a black square", () => {
+    expect(getPrevAcrossClueStart(0, 3, gridMock)).toEqual([0, 0]);
+  });
+  test("wrapping across rows", () => {
+    expect(getPrevAcrossClueStart(4, 5, gridMock)).toEqual([2, 0]);
+  });
+  test("wrapping to the previous row with a trailing black square", () => {
+    expect(getPrevAcrossClueStart(6, 1, gridMock)).toEqual([5, 4]);
+  });
+  test("wrapping back to the end", () => {
+    expect(getPrevAcrossClueStart(0, 1, gridMock)).toEqual([6, 4]);
+  });
+});
---
+++ app/utils/__test__/gridMock.js
@@ -0,0 +1,458 @@
+// [_, _, _, x, _, _, _]
+// [x, _, _, x, x, _, _]
+// [_, _, _, _, _, _, _]
+// [x, _, x, _, x, _, x]
+// [_, _, _, _, _, _, _]
+// [_, _, x, x, _, _, x]
+// [_, _, _, x, _, _, _]
+
+module.exports = [
+  [
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: false,
+        acrossClueNumber: 1,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isDownStart: true,
+        downClueNumber: 2,
+        acrossClueNumber: 1,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isDownStart: true,
+        downClueNumber: 3,
+        acrossClueNumber: 1,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: false,
+        acrossClueNumber: 4,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isDownStart: true,
+        downClueNumber: 5,
+        acrossClueNumber: 4,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isDownStart: true,
+        downClueNumber: 6,
+        acrossClueNumber: 4,
+      },
+      style: null,
+    },
+  ],
+  [
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: 2,
+        acrossClueNumber: 7,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 3,
+        acrossClueNumber: 7,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: 5,
+        acrossClueNumber: 8,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 6,
+        acrossClueNumber: 8,
+      },
+      style: null,
+    },
+  ],
+  [
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: false,
+        acrossClueNumber: 9,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 2,
+        acrossClueNumber: 9,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 3,
+        acrossClueNumber: 9,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isDownStart: true,
+        downClueNumber: 10,
+        acrossClueNumber: 9,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: false,
+        acrossClueNumber: 9,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 5,
+        acrossClueNumber: 9,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 6,
+        acrossClueNumber: 9,
+      },
+      style: null,
+    },
+  ],
+  [
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 2,
+        acrossClueNumber: false,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 10,
+        acrossClueNumber: false,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 5,
+        acrossClueNumber: false,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+  ],
+  [
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        isDownStart: true,
+        downClueNumber: 11,
+        acrossClueNumber: 11,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 2,
+        acrossClueNumber: 11,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: false,
+        acrossClueNumber: 11,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 10,
+        acrossClueNumber: 11,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isDownStart: true,
+        downClueNumber: 12,
+        acrossClueNumber: 11,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 5,
+        acrossClueNumber: 11,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: false,
+        acrossClueNumber: 11,
+      },
+      style: null,
+    },
+  ],
+  [
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: 11,
+        acrossClueNumber: 13,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 2,
+        acrossClueNumber: 13,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: 12,
+        acrossClueNumber: 14,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 5,
+        acrossClueNumber: 14,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+  ],
+  [
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: 11,
+        acrossClueNumber: 15,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 2,
+        acrossClueNumber: 15,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: false,
+        acrossClueNumber: 15,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: true,
+      clue: false,
+      style: null,
+      pencil: "",
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        isAcrossStart: true,
+        downClueNumber: 12,
+        acrossClueNumber: 16,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: 5,
+        acrossClueNumber: 16,
+      },
+      style: null,
+    },
+    {
+      value: "",
+      isBlackSquare: false,
+      clue: {
+        downClueNumber: false,
+        acrossClueNumber: 16,
+      },
+      style: null,
+    },
+  ],
+];
