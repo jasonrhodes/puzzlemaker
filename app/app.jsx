4d235fa9be934b9aa6f7bb9d6882098fb213c4c2
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2021-02-05 11:32:07 -0500

    Testing a change to see if GH actions work for CI (#6)

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ const ReactDOM = require('react-dom');
 const Route = require('react-router-dom').Route;
 const BrowserRouter = require('react-router-dom').BrowserRouter;
 
+// testing a change to see if it triggers a test run in the PR
+
 /* Import Components */
 const Builder = require('./components/Builder');
 const Home = require('./components/Home');
