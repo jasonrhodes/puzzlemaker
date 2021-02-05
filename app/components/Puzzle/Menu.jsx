be926169c78781e1c18b49c9d7815b318f88f476
Glitch (puzzlemaker) <none>
2021-02-05 04:13:24 +0000

    👽👱 Checkpoint ./app/components/Puzzle/Menu.jsx:27649419/5 ./static/style.css:27649419/4

--- app/components/Puzzle/Menu.jsx
+++ app/components/Puzzle/Menu.jsx
@@ -17,7 +17,7 @@ const SymmetryToggle = ({ symmetry, toggleSymmetry }) => {
     return (
       <a class="subicon" onClick={toggleLock}>
         <Lock size={18} />
-        <MirrorIcon size={8} />
+        <MirrorIcon size={10} />
         <span class="pbtip">
           <b>Unlock symmetry</b>
         </span>
@@ -27,7 +27,7 @@ const SymmetryToggle = ({ symmetry, toggleSymmetry }) => {
     return (
       <a class="subicon" onClick={toggleLock}>
         <Unlock size={18} />
-        <MirrorIcon size={12} />
+        <MirrorIcon size={10} />
         <span class="pbtip">
           <b>Lock symmetry</b>
         </span>
