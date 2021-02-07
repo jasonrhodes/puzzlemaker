546ff4c22553db8a3b2272ee86890f29e0b9e54b
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2021-02-07 00:56:52 -0500

    Puzzle tests!! (#13)
    * Updated cell navigation which fixes the broken test
    
    * Trying to figure out how to do real react tests and failed
    
    * Fixing files for baseline Puzzle tests

--- app/components/Puzzle/__test__/Context.test.js
+++ app/components/Puzzle/__test__/Context.test.js
@@ -1,17 +1,41 @@
 const React = require("react");
-const { render } = require("@testing-library/react");
+require("@testing-library/jest-dom/extend-expect");
+const { render, screen } = require("@testing-library/react");
+const userEvent = require("@testing-library/user-event").default;
 const { PuzzleContextProvider, PuzzleContext } = require("../Context");
 const initGrid = require("../../../utils/initGrid");
-
 const initialGrid = initGrid({ rows: 15, columns: 15 });
-test("does this work", () => {
-  const result = render(
-    <PuzzleContextProvider initialGrid={initialGrid}>
-      <PuzzleContext.Consumer>
-        {(puzzle) => {
-          <h1>puzzle.title</h1>;
-        }}
-      </PuzzleContext.Consumer>
-    </PuzzleContextProvider>
-  );
+
+const Wrapper = ({ children }) => (
+  <PuzzleContextProvider initialGrid={initialGrid}>
+    {children}
+  </PuzzleContextProvider>
+);
+
+describe("setting the active cell", () => {
+  const ActiveCell = ({ row, column }) => {
+    const puzzle = React.useContext(PuzzleContext);
+    const handleClick = () => {
+      puzzle.setActiveCell([row, column]);
+    };
+    return (
+      <React.Fragment>
+        <h1 data-testid="active-cell">{puzzle.activeCell.toString()}</h1>
+        <button data-testid="change" onClick={handleClick}>
+          Change Active Cell
+        </button>
+      </React.Fragment>
+    );
+  };
+
+  test("changing the active cell to a valid coordinate set", () => {
+    render(
+      <Wrapper>
+        <ActiveCell row={12} column={5} />
+      </Wrapper>
+    );
+    expect(screen.getByTestId("active-cell")).toHaveTextContent("");
+    userEvent.click(screen.getByTestId("change"));
+    expect(screen.getByTestId("active-cell")).toHaveTextContent("12,5");
+  });
 });
