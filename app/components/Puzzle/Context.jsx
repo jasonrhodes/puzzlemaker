dd11a05bf05fe52b7f4182ee056dd17a10c79563
Patrick Simon <patrick.simon@contact-software.com>
2021-02-07 18:26:33 +0100

    Remove unneeded rebuses when deselecting

--- app/components/Puzzle/Context.jsx
+++ app/components/Puzzle/Context.jsx
@@ -89,7 +89,12 @@ const PuzzleContextProvider = ({ initialGrid, puzzleId, children }) => {
 	  if (grid[prevCell[0]][prevCell[1]].isRebus && grid[prevCell[0]][prevCell[1]].value.length <= 1){
 		grid[prevCell[0]][prevCell[1]].isRebus = false;
 	  }
-    }
+    } else if (prevCell && prevCell.length && prevCell[0] !== undefined) {
+	  if (grid[prevCell[0]][prevCell[1]].isRebus && grid[prevCell[0]][prevCell[1]].value.length <= 1){
+		grid[prevCell[0]][prevCell[1]].isRebus = false;
+	  }
+	}
+	
   }, [activeCell]);
   
   React.useEffect(() => {
