82f81abf6a71e91adeae75adffc6e82e91413561
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2021-02-09 19:08:21 -0500

    Load PDF on demand, link to PDF in browser

--- app/components/PDF/Link.jsx
+++ app/components/PDF/Link.jsx
@@ -1,11 +1,26 @@
 const React = require("react");
-const { PDFDownloadLink } = require("@react-pdf/renderer");
+const { BlobProvider } = require("@react-pdf/renderer");
 const PDFPuzzleDocument = require("./PuzzleDocument");
 
+// { blob: null, url: null, loading: true, error: null }
+
+const Redirect = ({ url }) => {
+  window.location.href = url;
+  return null;
+}
+
 module.exports = function PDFLink({ puzzle, children }) {
+  const [load, setLoad] = React.useState(false);
+  if (!load) {
+    return (
+      <a onClick={() => setLoad(true)} style={{ cursor: "pointer" }}>
+        {children}
+      </a>
+    );
+  }
   return (
-    <PDFDownloadLink document={<PDFPuzzleDocument puzzle={puzzle} />} fileName={`${puzzle.title}.pdf`} >
-      {children}
-    </PDFDownloadLink>
+    <BlobProvider document={<PDFPuzzleDocument puzzle={puzzle} />} fileName={`${puzzle.title}.pdf`} >
+      {({ loading, url }) => loading ? children : <Redirect url={url} />}
+    </BlobProvider>
   );
 }
