1e1f6877cda225401222d81be4b7274c6a7fc035
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2021-02-16 14:13:07 -0500

    Fixes clearAll v clearAllPencils rename bugs (#33)

--- app/components/Puzzle/Cell.jsx
+++ app/components/Puzzle/Cell.jsx
@@ -29,7 +29,7 @@ const PuzzleCell = ({ cell, row, column, puzzle }) => {
       puzzle.setActiveCell([row, column]);
     }
     if (e.metaKey || e.ctrlKey) {
-      puzzle.clearAll(false);
+      puzzle.clearAllPencils(false);
       puzzle.toggleBlackSquare(row, column);
     }
     if (e.altKey) {
@@ -51,7 +51,7 @@ const PuzzleCell = ({ cell, row, column, puzzle }) => {
     const currentCell = puzzle.grid[activeRow][activeColumn];
     e.preventDefault();
     if (e.key === ".") {
-      puzzle.clearAll(false);
+      puzzle.clearAllPencils(false);
       puzzle.toggleBlackSquare(activeRow, activeColumn);
       return;
     }
